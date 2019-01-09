42aacdcf37694fb4e4142daa64bc142ab987d443
purcy <purcyguo@gmail.com>
2019-01-09 21:13:33 +0800

    reslize dropRight

--- Array/dropRight.js
+++ Array/dropRight.js
@@ -1,9 +1,9 @@
-Array.prototype.drop = function (value = 1) {
+Array.prototype.dropRight = function (value = 1) {
   value = Math.max(value, 0)
   if (value >= this.length) return []
   const result = new Array(this.length - value)
   for (let i = 0; i < result.length; i ++) {
-    result[i] = this[value + i]
+    result[i] = this[i]
   }
   return result
 }
