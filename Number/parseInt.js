b3d2f7de0ed4264831618349461fcdc901883dba
purcy <purcyguo@gmail.com>
2019-01-11 08:52:06 +0800

    modified parseInt error

--- Number/parseInt.js
+++ Number/parseInt.js
@@ -37,34 +37,48 @@ const radixTable = {
   z: 35
 }
 Number.parseInt = function (value, radix) {
+  let sign = 1
   if (typeof value !== 'string') value = value.toString()
+  value = trimLeft(value)
+  value = value.replace(/^([+-])?/, (match, $1) => {
+    if ($1 === '-') sign = -1
+    return ''
+  })
+  console.log(value)
   if (radix == null) {
-    if(/^0[xX]/.test(value)) {
-      return radixTransform(value.slice(2),16)
+    const patten = /^0([xXOoBb])/
+    if (patten.test(value)) {
+      value = value.replace(patten, (match, $1) => {
+
+        $1 = $1.toLowerCase()
+        if ($1 === 'x') radix = 16
+        else if ($1 === 'o') radix = 8
+        else if ($1 === 'b') radix = 2
+        return ''
+      })
     } else {
       radix = 10
     }
   } else {
-    if (typeof radix !== 'number') return NaN
+    radix = Number(radix)
     radix = Math.floor(radix)
     radix = isFinite(radix) ? radix : 10
     radix = isNaN(radix) ? 10 : radix
+    radix = radix === 0 ? 10 : radix
     if (radix == 1 || radix > 36) return NaN
   }
-  console.log('radix '+ radix)
-  return radixTransform(value, radix)
+  return radixTransform(value, radix) * sign
 }
 
 function radixTransform(value, radix) {
   value = value.split('')
   const digits = []
-  for (let i = 0; i < value.length; i ++) {
+  for (let i = 0; i < value.length; i++) {
     const digit = value[i]
-    if(digit in radixTable && radixTable[digit] < radix) digits.push(digit)
+    if (digit in radixTable && radixTable[digit] < radix) digits.push(digit)
     else break
   }
-  if(digits.length <= 0) return NaN
-  console.log("digits: "+ digits)
+  if (digits.length <= 0) return NaN
   let digitsNum = 1;
   const total = digits.reduceRight((accu, item) => {
     let value = radixTable[item] * digitsNum
@@ -75,3 +89,8 @@ function radixTransform(value, radix) {
   return total
 }
 
+function trimLeft(value) {
+  return value.replace(/^\s*/, '')
+}
+
+console.log(Number.parseInt('-13', 4))
