bc99d4b299ff1d15b2eacf479a5bf6b10625c4a1
purcy <purcyguo@gmail.com>
2019-01-11 16:27:21 +0800

    add CopyArray

---
+++ util/CopyArray.js
@@ -0,0 +1,12 @@
+function CopyArray(source, array) {
+  let index = -1
+  const length = source.length
+  array || (array = new Array(length))
+  while(++index < length) {
+    array[index] = source[index]
+  }
+  return array
+
+}
+
+export default CopyArray
