f3228cf8e642d4b0b99b2f57560a840aa4cc197f
purcy <purcyguo@gmail.com>
2019-01-25 23:10:29 +0800

    add everyValue.js

---
+++ internal/everyValue.js
@@ -0,0 +1,10 @@
+function everyValue(object, predicate) {
+  object = Object(object)
+  const props = Object.keys(object)
+
+  for (const key of props) {
+    if(!predicate(object[key], key, object)) return false
+  }
+
+  return true
+}
